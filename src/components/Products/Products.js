c4202a567bccde57a630591564196a63ba0204f6
Chey Rose <cheyroseflammer@gmail.com>
2021-03-24 12:13:56 -0700

    products importing from commerce js database

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,26 +3,7 @@ import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
 import useStyles from './styles'
 
-const products = [
-  {
-    id: 1,
-    name: 'Shoes',
-    description: 'Running shoes',
-    price: '$5',
-    image:
-      'https://images-na.ssl-images-amazon.com/images/I/711RJEthqtL._AC_UL1500_.jpg',
-  },
-  {
-    id: 2,
-    name: 'Macbook',
-    description: 'Apple macbook',
-    price: '$1800',
-    image:
-      'https://cnet3.cbsistatic.com/img/yjrw7VgWV7a95AvK8Ym0Np4bFXY=/1200x675/2017/06/27/13484418-bfd9-41e2-8f2d-9b4afb072da8/apple-macbook-pro-15-inch-2017-14.jpg',
-  },
-]
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles()
   return (
     <main className={classes.content}>
