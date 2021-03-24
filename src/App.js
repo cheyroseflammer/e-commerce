82e44b62a3f97dac0fc842ca521cb32262f4b61a
Chey Rose <cheyroseflammer@gmail.com>
2021-03-24 09:31:38 -0700

    initalizing commerce js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Products, NavBar } from './components'
+import { commerce } from './library/commerce'
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list()
+
+    setProducts(data)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  console.log(products)
   return (
     <div className='App'>
       <NavBar />
